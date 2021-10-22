bbc64fc99071f99ceb51f0548e58f50be0defc60
EC2 Default User <ayasaku.t5@gmail.com>
2021-10-22 05:38:38 +0000

    [Add]add controllers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get '/homes/about' => 'homes#about'
   devise_for :users
+  resources :books, expect:[:new]
   
 end
