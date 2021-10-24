2a2131c23588a4ffc81e1480850c2370b972eef0
ayano-w <ayasaku.t5@gmail.com>
2021-10-24 06:41:00 +0000

    [Add]add book/show and index html files.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,36 @@
 class BooksController < ApplicationController
-  
+
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
-  
+
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = User.find(current_user[:id])
   end
-  
+
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
-  
+
   def edit
   end
-  
+
   def destroy
   end
-  
+
   def update
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   attachment :profile_image
   
   has_many :books, dependent: :destroy
+  attachment  :profile_image
   
 end
