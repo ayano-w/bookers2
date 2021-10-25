c96cd9547ea2401126b122ed3bb8a1ecb4227129
ayano-w <ayasaku.t5@gmail.com>
2021-10-25 11:13:32 +0000

    [Finish]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      flash[:notice] = 'You have updated user succeccfully.'
+      flash[:notice] = 'You have updated user successfully.'
       redirect_to user_path(@user.id)
     else
       render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get '/homes/about' => 'homes#about'
+  get '/home/about' => 'homes#about'
   devise_for :users
   resources :books, expect: [:new]
   resources :users, only: [:index, :show, :edit, :update]
-  
+
 end
